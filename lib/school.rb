f45926237c6f4ae007995997abbe884cff1ae545
Amanda Rolon <abrolon87@gmail.com>
2019-12-27 05:49:58 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -2,13 +2,26 @@
 class School
   attr_accessor :name, :roster
 
-  def initialize(school)
-    @school = school
+  def initialize(name)
+    @name = name
     @roster = {}
   end
 
-  def add_student
-    
+  def add_student(student_name, grade)
+    roster[grade] ||= []
+    roster[grade] << student_name
+  end
+
+  def grade(student_grade)
+    roster[student_grade]
+  end
+
+  def sort
+    sorted = {}
+    roster.each do |grade, student|
+      sorted[grade] = student.sort
+    end
+    sorted
   end
 
 end
