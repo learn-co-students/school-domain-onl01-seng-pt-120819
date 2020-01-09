1b74ca7b46c518a0879a66a5bdaa6b4fb8b8966f
S. Makai Takori <makait87@gmail.com>
2020-01-09 02:05:39 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
-# code here!
+class School
+  
+attr_accessor :name, :roster  
+
+  def initialize(name, roster = {})
+    @name = name
+    @roster = roster
+      
+  end  
+
+  def add_student(name, grade)
+    @student_name = name
+    @grade = grade
+    
+    # ||= is equivalent to a || a = b; if a returns nil or false then a is set to b
+    
+    roster[grade] ||= []
+    roster[grade] << name 
+  end
+  
+  def grade(grade)
+    roster[grade]
+  end
+  
+  def sort
+    roster.each do |grade, name|
+      roster[grade] = name.sort
+    end  
+  end
+end  
